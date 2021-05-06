64b7f62cb0dabd021d55e7adb23e6406192a955a
Tristan Miller <tristanmiller89@gmail.com>
2021-05-05 22:46:15 -0500

    removed old code

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 const Todo = (props) => {
   return (
-    // <div
-    //   onClick={handleClick}
-    //   className={`todo${props.todo.completed ? ' completed' : ''}`}>
-    //   <p>{props.todo.name}</p>
-    // </div>
     <div
       onClick={() => props.toggleCompleted(props.item.id)}
       className={`todo${props.item.completed ? ' completed' : ''}`}>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,10 @@
 import React from 'react';
 
 const TodoForm = (props) => {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     newTodoName: '',
-  //   };
-  // }
-
   return (
     <div>
       <form onSubmit={props.handleSubmit}>
-        <input
-          onChange={props.handleChanges}
-          // value={props.handleChanges}
-          name="new-todo"
-          type="text"
-        />
+        <input onChange={props.handleChanges} name="new-todo" type="text" />
         <button>Add Todo</button>
       </form>
       <button onClick={() => props.clearCompleted()}>Clear Completed</button>
